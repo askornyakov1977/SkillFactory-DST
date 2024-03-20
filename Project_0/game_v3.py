b283ccb69e0cd9d2f15d53999bf8b291bd929e7c
askornyakov1977 <askornyakov1977@mail.ru>
2024-03-20 05:53:32 +0300

    init_my

---
+++ Project_0/game_v3.py
@@ -0,0 +1,111 @@
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict_number:
+            break  # выход из цикла если угадали
+
+    return count
+
+
+def game_core_v2(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, а потом уменьшаем
+    или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+
+    return count
+
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Инициализируем счетчик попыток угадывания начальное значение
+    count = 0
+    # Инициализируем начало и конец искомого диапазона
+    first = 1
+    last = 100
+    # Создаем цикл, проверяем пока искомый диапазон
+    while first <= last:
+        # Инкрементируем счетчик
+        count += 1
+        # Присвоим предсказательному числу среднее значение от искомого диапазона
+        predict = first + (last - first) // 2
+        # Если предсказательное число и загаданное равны, тогда возвращаем значение счетчка попыток
+        if predict == number:
+            return count
+        # Если предсказательное число меньше загаданного — продолжаем поиск в правой части массива, иначе в левой
+        elif predict < number:
+            first = predict + 1
+        else:
+            last = predict - 1
+    # Возвращаем отрицательный результат если загаданное число оказалось вне диапазона от 1 до 100
+    return -1
+
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+
+
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(
+        1, 101, size=(10))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+
+
+# Run benchmarking to score effectiveness of all algorithms
+print('Run benchmarking for random_predict: ', end='')
+score_game(random_predict)
+
+print('Run benchmarking for game_core_v2: ', end='')
+score_game(game_core_v2)
+
+print('Run benchmarking for game_core_v3: ', end='')
+score_game(game_core_v3)
---
+++ README.md
@@ -0,0 +1,10 @@
+# My data science projects
+From the [Skillfactory Data Science course](https://skillfactory.ru/data-scientist)
+
+## Проекты
+
+* [Проект 0. Игра: Угадай число](https://github.com/askornyakov1977/Skillfactory-DST/tree/main/Project_0)
+* [Проект 1. Анализ резюме из HeadHunter](https://github.com/askornyakov1977/Skillfactory-DST/tree/main/Project_1)
+* [Проект 2. Анализ вакансий из HeadHunter](https://github.com/askornyakov1977/Skillfactory-DST/tree/main/Project_2)
+* [Проект 3. Предсказывание рейтинга отеля](https://github.com/askornyakov1977/SkillFactory-DST/tree/main/Project_3)
+* [Проект 4. Классификация открытия депозита](https://github.com/askornyakov1977/SkillFactory-DST/tree/main/Project_4)
